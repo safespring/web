acc9729d30a77edab2217b3cafb060c8609f06a2
Marcus Boberg <38211830+marcusboberg@users.noreply.github.com>
2023-01-20 12:35:48 +0100

    Installed Safespring analytics tag manager

---
+++ static/js/matomo-tagmanager.js
@@ -0,0 +1,4 @@
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.async=true; g.src='https://analytics.safespring.com/js/container_43nGFJfy.js'; s.parentNode.insertBefore(g,s);
