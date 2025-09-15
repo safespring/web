9627bc4195bfaf1763cb488100e1d9636ffac696
Felix Schulze <felix.schulze@safespring.com>
2025-09-15 16:51:29 +0200

    nix-shell fix

--- scripts/shell.nix
+++ scripts/shell.nix
@@ -4,7 +4,7 @@ in pkgs.mkShell {
   packages = [
     (pkgs.python3.withPackages (python-pkgs: [
       python-pkgs.openai
-      python-pkgs.dotenv
+      python-pkgs.python-dotenv
     ]))
   ];
 }
