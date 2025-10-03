4022c95baef6c736364d86ed8f0c5b959eb763f4
Felix Schulze <felix.schulze@safespring.com>
2025-10-03 13:16:34 +0200

    Matomo Site ID
    let Hugo statically fill in the matomo site id

--- static/js/matomo.js
+++ static/js/matomo.js
@@ -5,7 +5,7 @@
   (function() {
     var u="https://analytics.safespring.com/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
+    _paq.push(['setSiteId', matomoSiteId]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
